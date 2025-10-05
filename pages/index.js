e8955357953c5d09a6b5e6061f9fe4e9a3f84bff
Matheus Douglas Almeida Reis <116857822+g-matheusdouglas@users.noreply.github.com>
2025-10-05 15:34:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Muito obrigado pelo trabalho de todos vocês 🙏 </h1>
+  return <h1>Muito obrigado pelo trabalho de todos vocês 🙏 </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("Indentação errada");
 }
 
 export default Home;
